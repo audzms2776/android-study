a77cec6183aac6fa66539aa15ef9289e8dd61aa9
ss <diddydwns135@gmail.com>
2016-11-14 17:51:50 +0900

    ss

--- NaverCaptcha/app/src/main/java/com/example/user/myapplication/MyConstant.java
+++ NaverCaptcha/app/src/main/java/com/example/user/myapplication/MyConstant.java
@@ -9,6 +9,6 @@ public class MyConstant {
     public static final String NAVER_KEY_URL = "https://openapi.naver.com/v1/captcha/nkey?";
     public static final String NAVER_CAPTCHA_URL = "https://openapi.naver.com/v1/captcha/ncaptcha.bin?";
     public static String NAVER_CHAPTCHA_KEY = "";
-    public static final String X_Naver_Client_Id = "a1RZIb1p59TLbq3iu_un";
-    public static final String X_Naver_Client_Secret = "yzvyDMQvAL";
+    public static final String X_Naver_Client_Id = "X-NAVER-CLIENT-ID";
+    public static final String X_Naver_Client_Secret = "X-NAER-CLIENT-SECRET";
 }
